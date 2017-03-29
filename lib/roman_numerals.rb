8c8501b3d04f4923c84e7983303defe776b1c981
katerina-codes <katerinaldg@gmail.com>
2017-03-29 10:27:12 +0100

    Second test to convert 1 to I is green.

--- lib/roman_numerals.rb
+++ lib/roman_numerals.rb
@@ -1,7 +1,11 @@
 class RomanNumerals
 
 def convert_arabic_number(number)
-  " "
+  if number == 0
+    " "
+  else
+    "I"
+  end  
 end
 
 end
--- spec/roman_numerals_spec.rb
+++ spec/roman_numerals_spec.rb
@@ -2,14 +2,14 @@ require 'roman_numerals'
 
 describe RomanNumerals do
 
-  it "should return nothing if the arabic number is zero" do
+  it "returns nothing if the arabic number is 0" do
      number_to_numeral = RomanNumerals.new
      expect(number_to_numeral.convert_arabic_number(0)).to eq(" ")
   end
 
-  # it "should return a roman numeral when given an arabic number" do
-  #   number_to_numeral = RomanNumerals.new
-  #   expect(number_to_numeral.convert_arabic_number(1)).to eq('I')
-  # end
-  
+  it "returns I if the arabic number is 1" do
+    number_to_numeral = RomanNumerals.new
+    expect(number_to_numeral.convert_arabic_number(1)).to eq('I')
+  end
+
 end
